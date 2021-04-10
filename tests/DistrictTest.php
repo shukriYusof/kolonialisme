d8782847f0a5adabcb3dd1cd491811f63752faba
Shukri Yusof <shukriyusof65@gmail.com>
2021-04-10 14:18:38 +0800

    update php unit test file and add unit test for district and school

--- tests/DistrictTest.php
+++ tests/DistrictTest.php
@@ -4,5 +4,19 @@ require_once ('index.php');
 
 final class DistrictTest extends TestCase
 {
+    public function testGetAllDistricts(){
+        return $this->assertIsArray(District::getAllDistricts());
+    }
 
+    public function testGetAllDistrictsById(){
+        return $this->assertIsArray(District::getDistrictById(59));
+    }
+
+    public function testGetAllDistrictsByName(){
+        return $this->assertIsArray(District::getDistrictByName("PPD KUBANG PASU"));
+    }
+
+    public function testGetAllDistrictsByStateId(){
+        return $this->assertIsArray(District::getDistrictByStateId(2));
+    }
 }
--- tests/SchoolTest.php
+++ tests/SchoolTest.php
@@ -4,5 +4,93 @@ require_once ('index.php');
 
 final class SchoolTest extends TestCase
 {
- 
+    public function testGetAllSchool(){
+        return $this->assertIsArray(School::getAllSchools());
+    }
+
+    public function testGetAllSchoolById(){
+
+        $res = School::getSchoolById(6616);
+
+        $expected = [
+            "id" => "6616",
+            "district_id" => "124",
+            "name" => "SEKOLAH KEBANGSAAN SENTOSA SIBU",
+            "schoolcode" => "YBA3105",
+            "level" => "Rendah",
+            "address" => "LOT 513 BLOK 9 JALAN SENTOSA OFF JALAN SALIM ",
+            "postcode" => "96000",
+            "city" => "SIBU",
+            "email" => "YBA3105@moe.edu.my",
+            "coordinatexx" => "111.8652667",
+            "coordinateyy" => "2.270433333"
+        ];
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+    public function testGetSchoolByLevel() {
+        $this->assertIsArray(School::getSchoolByLevel("Menengah"));
+    }
+
+    public function testGetSchoolByDistrictId() {
+        $this->assertIsArray(School::getSchoolByDistrictId(1));
+    }
+
+    public function testGetSchoolByCity() {
+        $this->assertIsArray(School::getSchoolByCity("CHENDERIANG"));
+    }
+
+    public function testGetSchoolByPostcode() {
+        $this->assertIsArray(School::getSchoolByPostcode(35000));
+    }
+
+    public function testGetCountAllSchools(){
+
+        $res = School::getCountAllSchools();
+
+        $expected = 10218;
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+    public function testGetCountSchoolByLevel(){
+
+        $res = School::getCountSchoolByLevel("Menengah");
+
+        $expected = 2439;
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+    public function testGetCountSchoolByDistrictId(){
+
+        $res = School::getCountSchoolByDistrictId(1);
+
+        $expected = 122;
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+    public function testGetCountSchoolByCity(){
+
+        $res = School::getCountSchoolByCity("CHENDERIANG");
+
+        $expected = 4;
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+    public function testGetCountSchoolByPostcode(){
+
+        $res = School::getCountSchoolByPostcode(35000);
+
+        $expected = 19;
+
+        return $this->assertEquals( $expected, $res );
+    }
+
+
+
+    
 }
